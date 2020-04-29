7cb37103b55dee0c55b769c320dcf91f97357129
joeriLock93 <54021976+joeriLock93@users.noreply.github.com>
2020-04-29 10:33:29 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -3,5 +3,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'ReachDigital_BetterIndexers',
-    __DIR__
+    __DIR__ . '/src'
 );
